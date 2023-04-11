b1f1a09538a512abd3c38bba78eed83beffae501
champiuned12 <champiuned12@gmail.com>
2023-04-11 20:03:36 +0900

    register page done

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -12,8 +12,16 @@
           <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="state.form.password">
           <label for="floatingPassword">Password</label>
         </div>
+        <div class="form-floating">
+          <input type="name" class="form-control" id="floatingName" placeholder="Name" v-model="state.form.name">
+          <label for="floatingName">Name</label>
+        </div>
+        <div class="form-floating">
+          <input type="phonenum" class="form-control" id="floatingPhone" placeholder="Phonenum" v-model="state.form.phonenum">
+          <label for="floatingPhone">Phonenum</label>
+        </div>
 
-        <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign Up</button>
+      <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign Up</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2017–2023</p>
 
     </div>
@@ -71,7 +79,21 @@ export default{
 }
 
 .form-signin input[type="password"] {
+  margin-bottom: -1px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+.form-signin input[type="name"] {
+  margin-bottom: -1px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+.form-signin input[type="phonenum"] {
   margin-bottom: 10px;
   border-top-left-radius: 0;
   border-top-right-radius: 0;
-}</style>
+}
+
+
+
+</style>
